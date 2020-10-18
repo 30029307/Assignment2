dfa854b140e43761b39d3a9aadeee581a55088d6
Derick John Aguiling <30029307@student.toiohomai.ac.nz>
2020-10-18 20:00:13 +0800

    first commit

---
+++ Login.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Assignment_2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Login : Page
+    {
+        public Login()
+        {
+            this.InitializeComponent();
+
+           
+        }
+
+       
+
+        private void GoRegister_Click(Windows.UI.Xaml.Documents.Hyperlink sender, Windows.UI.Xaml.Documents.HyperlinkClickEventArgs args)
+        {
+           Frame.Navigate(typeof(Registration));
+        
+        }
+
+        private async  void buttonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog msg = new MessageDialog("", "");
+
+            var userQuery = App.user.Where(u => u.Username.Equals(textBoxUsername.Text) && u.Password.Equals(textBoxPass.Password)).FirstOrDefault();
+
+            if (userQuery == null)
+            {
+                msg.Content = "Invalid Account";
+                msg.Title = "Login Failed";
+            }
+            else
+            {
+                msg.Content = String.Format("Welcome {0}",userQuery.Username);
+                msg.Title = "Login Successful";
+            }
+           await msg.ShowAsync();
+        }
+    }
+}
---
+++ Registration.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using Windows.UI.Popups;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Assignment_2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Registration : Page
+    {
+        
+
+      //  public static  List<User> user = new List<User>();
+
+
+        public Registration()
+        {
+            this.InitializeComponent();
+
+           
+        }
+
+        private void buttonLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            Frame.Navigate(typeof(Login));
+
+        }
+
+        private async void buttonRegister_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog msg = new MessageDialog("");
+
+            if (textBoxUsername.Text == "" &&
+                textBoxLastname.Text == "" &&
+                textBoxUsername.Text == "" &&
+                textBoxPassword.Password == "" &&
+                textBoxEmail.Text == "" &&
+                textBoxContact.Text == "")
+            {
+
+                msg.Content = "Please complete details.";
+
+            }
+            else if (textBoxUsername.Text != "" &&
+                     textBoxLastname.Text != "" &&
+                     textBoxUsername.Text != "" &&
+                     textBoxPassword.Password != "" &&
+                     textBoxEmail.Text != "" &&
+                     textBoxContact.Text != "") {
+                App.user.Add(new User
+                {
+                    FirstName = textBoxFirstname.Text,
+                    LastName = textBoxLastname.Text,
+                    Username = textBoxUsername.Text,
+                    Password = textBoxPassword.Password,
+                    Email = textBoxEmail.Text,
+                    Contact = textBoxContact.Text,
+                });
+
+                msg.Content = "Registraition Successfully";
+                Frame.Navigate(typeof(Login));
+
+            }
+            else { msg.Content = "Please complete details."; }
+
+
+
+            await msg.ShowAsync();
+        }
+
+     
+    }
+}
